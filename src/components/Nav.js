d997fe11d72ff3d29e0f33b60e00bb53da1b1445
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-09-18 11:16:10 -0700

    created nav component, added divs

---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+function Nav() {
+    return (
+      <div className="nav">
+        <div className="burger-menu"></div>
+        <div className="logo"></div>
+        <div className='contact'></div>
+      </div>
+    );
+  }
+  
+  export default Nav;
