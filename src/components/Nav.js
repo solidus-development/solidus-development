18116889a226b7f335162e739472923a77443153
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-09-18 11:25:11 -0700

    installed react-icons, added burger menu icon to nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,11 @@
+import { FaBars } from 'react-icons/fa';
+
 function Nav() {
     return (
       <div className="nav">
-        <div className="burger-menu"></div>
+        <div className="burger-menu">
+            <FaBars />
+        </div>
         <div className="logo"></div>
         <div className='contact'></div>
       </div>
