e873a67fcaaaffc4b325cd4c4c57911e8426f359
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-09-22 20:58:38 -0700

    removed color of navbar, added bottom black border

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,12 +5,12 @@ import Navbar from "react-bootstrap/Navbar";
 function Navigation() {
   return (
     <div>
-      <Navbar bg="primary" expand="lg" variant="dark" fixed='top'>
+      <Navbar className='nav' fixed='top'>
         <Container>
           <Nav className="me-auto">
-            <Nav.Link href="#home">About</Nav.Link>
-            <Nav.Link href="#features">Portfolio</Nav.Link>
-            <Nav.Link href="#pricing">Testimonials</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+            <Nav.Link href="#testimonials">Testimonials</Nav.Link>
           </Nav>
           <Navbar.Brand href="#home">Daemon Development</Navbar.Brand>
         </Container>
