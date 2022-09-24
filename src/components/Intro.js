5f4e05d7aadbacb1e22cb4c4a1bb2f1d92260854
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-09-24 16:33:29 -0700

    added image and text to intro component

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -7,7 +7,11 @@ function Intro() {
       <div className='intro'>
         <Image src={MountainTop} alt='mountain-top' fluid />
         <Container>
-            Intro
+            <div className="intro-content">
+                <h1>Daemon Development</h1>
+                <h2>Servicing the entrepreneurial spirit</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean maximus molestie massa, quis commodo felis aliquam sed. Nullam pretium nulla vitae nisl porta semper. Nulla a blandit massa. Donec accumsan.</p>
+            </div>
         </Container>
       </div>
     );
