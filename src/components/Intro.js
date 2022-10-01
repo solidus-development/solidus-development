0aad9335409ef0d76a28296d906c1a45d7a0ba64
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-10-01 15:19:10 -0700

    removed intro image

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,11 +1,9 @@
 import { Container } from "react-bootstrap";
 import Image from "react-bootstrap/Image";
-import MountainTop from '../images/mountain-top.png'
 
 function Intro() {
     return (
       <div className='intro'>
-        <Image src={MountainTop} alt='mountain-top' fluid />
         <Container>
             <div className="intro-content">
                 <h1>Daemon Development</h1>
