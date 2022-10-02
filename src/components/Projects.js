b682b65e1fd949d37238961054ca2bf9559fc3d7
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-10-02 11:27:43 -0700

    added image for project card

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import Image from "react-bootstrap/Image";
 import Card from 'react-bootstrap/Card';
 import Button from "react-bootstrap/Button";
+import seaweasel from '../images/seaweasel.png';
 
 function Projects() {
     return (
@@ -11,10 +12,10 @@ function Projects() {
             <Row>
                 <Col>
                     <Card>
-                        <Card.Img />
                         <Card.Body>
-                            <Card.Title>Title</Card.Title>
-                            <Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Text>
+                            <Card.Title>Sea Weasel</Card.Title>
+                            <Card.Img src={seaweasel}/>
+                            <Card.Text>Sea Weasel is a punk rock, nautical clothing line. This landing page collects user emails who want to be notified of product releases.</Card.Text>
                             <Button variant='dark'>Site</Button>
                             <Button variant='dark'>Code</Button>
                         </Card.Body>
