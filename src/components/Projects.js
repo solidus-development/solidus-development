0b1f332b14bf9636a978a08ce7936fb0fa572675
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-10-03 20:11:55 -0700

    added style to cards and created sectin-header class

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,22 +2,25 @@ import { Container, Row, Col } from "react-bootstrap";
 import Image from "react-bootstrap/Image";
 import Card from 'react-bootstrap/Card';
 import Button from "react-bootstrap/Button";
+import Stack from 'react-bootstrap/Stack';
 import seaweasel from '../images/seaweasel.png';
 
 function Projects() {
     return (
       <div className='projects component'>
         <Container>
-            <h2>Projects</h2>
+            <h2 className='section-header'>Projects</h2>
             <Row>
                 <Col>
-                    <Card>
+                    <Card border='dark' style={{ width: '16rem'}}>
                         <Card.Body>
-                            <Card.Title>Sea Weasel</Card.Title>
-                            <Card.Img src={seaweasel}/>
-                            <Card.Text>Sea Weasel is a punk rock, nautical clothing line. This landing page collects user emails who want to be notified of product releases.</Card.Text>
-                            <Button variant='dark'>Site</Button>
-                            <Button variant='dark'>Code</Button>
+                            <Card.Title style={{ marginBottom: '20px'}}>Sea Weasel</Card.Title>
+                            <Card.Img src={seaweasel} style={{ marginBottom: '20px'}}/>
+                            <Card.Text>Sea Weasel is a punk rock, nautical clothing line. This landing page collects users' emails who want to be notified of product releases.</Card.Text>
+                            <div className='card-buttons'>
+                                <Button variant='dark'>Site</Button>
+                                <Button variant='dark'>Code</Button>
+                            </div>
                         </Card.Body>
                     </Card>
                 </Col>
