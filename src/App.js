7508e596fcd7f6d83ff3e506f089509ac5c91553
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-10-06 10:55:23 -0700

    created About component and styled

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navigation from './components/Navigation';
 import Intro from './components/Intro';
 import Projects from './components/Projects';
 import CTA from './components/CTA';
+import About from './components/About';
 import Testimonials from './components/Testimonials';
 import Footer from './components/Footer';
 
@@ -14,6 +15,7 @@ function App() {
       <Intro />
       <Projects />
       <CTA />
+      <About />
     </div>
   );
 }
