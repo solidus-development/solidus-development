213a3e830be09c8070e705854266efadeb52ae33
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-10-13 20:13:11 -0700

    updated business name in Intro component

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -7,7 +7,7 @@ function Intro() {
         <div id='home'></div>
         <Container>
             <div className="intro-content">
-                <h1 className="glow">Daemon Development</h1>
+                <h1 className="glow">Solidus Development</h1>
                 <h2>Servicing the entrepreneurial spirit</h2>
                 <p>Affordable web design, web development, copywriting, and SEO.</p>
             </div>
