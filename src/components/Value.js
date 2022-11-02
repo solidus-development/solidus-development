48de4f820b2fe8bc9dc948e3beeb85f7cbc24ecb
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-11-01 19:04:27 -0700

    added value content structure

--- src/components/Value.js
+++ src/components/Value.js
@@ -1,4 +1,5 @@
 import { Container, Row, Col } from "react-bootstrap";
+import Image from "react-bootstrap/Image";
 import design from '../images/design.png';
 import development from '../images/development.png';
 import copy from '../images/copywriting.png';
@@ -11,7 +12,32 @@ function Value() {
             <h2 className="section-header">Why Solidus?</h2>
             <Row>
                 <Col>
-                    <h3></h3>
+                    <div className='value-card'>
+                        <h3 className="blue">Design</h3>
+                        <Image src={design} fluid />
+                        <p>Websites optimized for search results so more people see your value</p>
+                    </div>
+                </Col>
+                <Col>
+                    <div className='value-card'>
+                        <h3 className="blue">Design</h3>
+                        <Image src={design} fluid />
+                        <p>Websites optimized for search results so more people see your value</p>
+                    </div>
+                </Col>
+                <Col>
+                    <div className='value-card'>
+                        <h3 className="blue">Design</h3>
+                        <Image src={design} fluid />
+                        <p>Websites optimized for search results so more people see your value</p>
+                    </div>
+                </Col>
+                <Col>
+                    <div className='value-card'>
+                        <h3 className="blue">Design</h3>
+                        <Image src={design} fluid />
+                        <p>Websites optimized for search results so more people see your value</p>
+                    </div>
                 </Col>
             </Row>
         </Container>
