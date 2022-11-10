4b548e82281b284234700a064e4ed66732b546f8
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-11-10 12:01:41 -0800

    removed burger menu for larger screens and replaced with regular nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,13 +3,25 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Button } from "react-bootstrap";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { HashLink as Link } from 'react-router-hash-link';
 
 function Navigation(props) {
   return (
     <div>
       <Navbar className='nav' fixed='top'>
         <Container>
+
+        <ul className="nav-menu-list">
+            <li><Link smooth to='#home'>Home</Link></li>
+            <li><Link smooth to='#projects'>Projects</Link></li>
+            <li><Link smooth to='#contact'>Contact</Link></li>
+            <li><Link smooth to='#about'>About</Link></li>
+        </ul>
+
+          {/* FOR MOBILE */}
           <GiHamburgerMenu className='nav-burger' onClick={() => props.setMenu(true)} />
+          {/* --- */}
+
           <Button variant='dark' type='button' onClick={() => props.setModal(true)}>Contact</Button>
         </Container>
       </Navbar>
