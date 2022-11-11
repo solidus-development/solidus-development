ecebad30329950883adef29f3889c7b8d130b5e9
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-11-11 10:50:45 -0800

    added send button functionality to contact form

--- src/components/ContactModal.js
+++ src/components/ContactModal.js
@@ -22,7 +22,7 @@ const ContactModal = (props) => {
                   <input type="hidden" name="_next" value="https://damonpickett.github.io/damonpickett/thankyou.html" />
                   <input type="hidden" name="_subject" value="New enquiry from portfolio site" />
                   <textarea name="message" rows="5" placeholder="How can I help you?"></textarea>
-                  <Button variant='dark' type='button'>Send</Button>
+                  <Button variant='dark' type='submit'>Send</Button>
                   <p>Please feel free to connect with me on <a href='https://www.linkedin.com/in/damon-pickett/' target='_blank' rel='noreferrer'>Linkedin</a></p>
               </form>
           </div>
