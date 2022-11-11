d55e0fe7c92af6908c3674c7e71625f2c8412a31
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-11-11 11:51:33 -0800

    added credits to Social component

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,19 +1,33 @@
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import Stack from "react-bootstrap/Stack";
 
 function Social() {
   return (
     <div className="social component">
       <Container>
-        <div className="social-content">
-          <Stack direction="horizontal" className='social-stack'>
-            <a href='https://www.linkedin.com/in/damon-pickett/' target='_blank' rel='noreferrer'><i class="devicon-linkedin-plain"></i></a>
+        <Row>
+            <Col sm={12} md={6}>
+              <h3>Social</h3>
+              <div className="social-content">
+                <Stack direction="horizontal" className='social-stack'>
+                  <a href='https://www.linkedin.com/in/damon-pickett/' target='_blank' rel='noreferrer'><i class="devicon-linkedin-plain"></i></a>
 
-            <a href='https://github.com/damonpickett' target='_blank' rel='noreferrer'><i class="devicon-github-original"></i></a>
+                  <a href='https://github.com/damonpickett' target='_blank' rel='noreferrer'><i class="devicon-github-original"></i></a>
 
-            <a href='https://twitter.com/Damon_Pickett' target='_blank' rel='noreferrer'><i class="devicon-twitter-original"></i></a>
-          </Stack>
-        </div>
+                  <a href='https://twitter.com/Damon_Pickett' target='_blank' rel='noreferrer'><i class="devicon-twitter-original"></i></a>
+                </Stack>
+              </div>
+          </Col>
+          <Col sm={12} md={6}>
+            <h3>Credits</h3>
+            <ul className='credit-list'>
+              <li><a href="https://www.flaticon.com/free-icons/graphic-design" title="graphic design icons">Graphic design icons created by Freepik - Flaticon</a></li>
+              <li><a href="https://www.flaticon.com/free-icons/code" title="code icons">Code icons created by Icongeek26 - Flaticon</a></li>
+              <li><a href="https://www.flaticon.com/free-icons/copywriting" title="copywriting icons">Copywriting icons created by Freepik - Flaticon</a></li>
+              <li><a href="https://www.flaticon.com/free-icons/marketing" title="marketing icons">Marketing icons created by monkik - Flaticon</a></li>
+            </ul>
+          </Col>
+        </Row>
       </Container>
     </div>
   );
