7b2f1e6d691237a37db84392fe1dc315630230ba
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-11-26 10:49:14 -0800

    resized project images

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,12 +13,13 @@ function Projects() {
         <Container>
             <h2 className='section-header'>Projects</h2>
             <Row>
+
                 <Col className='projects-col' sm={12} md={6} lg={4}>
                 <div className="card-frame">
-                <Card border='dark' style={{ width: '16rem'}}>
+                <Card border='dark'>
                         <Card.Body>
                             <Card.Title style={{ marginBottom: '20px'}}>Sea Weasel</Card.Title>
-                            <Card.Img src={seaweasel} style={{ marginBottom: '20px'}}/>
+                            <Card.Img src={seaweasel} />
                             <Card.Text>Sea Weasel is a punk rock, nautical clothing line. This landing page collects users' emails who want to be notified of product releases.</Card.Text>
                             <div className='card-buttons'>
                                 <a href='https://seaweasel.ca/' target='_blank' rel='noreferrer'><Button variant='dark'>Site</Button></a>
@@ -28,12 +29,13 @@ function Projects() {
                     </Card>
                     </div>
                 </Col>
+
                 <Col className='projects-col' sm={12} md={6} lg={4}>
                 <div className="card-frame">
-                <Card border='dark' style={{ width: '16rem' }}>
+                <Card border='dark'>
                         <Card.Body>
                             <Card.Title style={{ marginBottom: '20px'}}>Torbomb Design</Card.Title>
-                            <Card.Img src={torbomb} style={{ marginBottom: '20px'}}/>
+                            <Card.Img src={torbomb}/>
                             <Card.Text>Torbomb Designs is expanding possibilities in the world of paintball by creating accessories via 3D printing technology.</Card.Text>
                             <div className='card-buttons'>
                             <a href='https://torbombdesigns.bigcartel.com/' target='_blank' rel='noreferrer'><Button variant='dark'>Site</Button></a>
@@ -42,12 +44,13 @@ function Projects() {
                     </Card>
                 </div>
                 </Col>
+
                 <Col className='projects-col' sm={12} md={6} lg={4}>
                 <div className="card-frame">
-                <Card border='dark' style={{ width: '16rem'}}>
+                <Card border='dark'>
                         <Card.Body>
                             <Card.Title style={{ marginBottom: '20px', fontSize: '1.2em'}}>The Whitsteen Project</Card.Title>
-                            <Card.Img src={whitsteen} style={{ marginBottom: '20px'}}/>
+                            <Card.Img src={whitsteen}/>
                             <Card.Text>The Whitsteen Project is a mock NFT project based on the artwork of Whitsteen. This app records transactions on the Ethereum testnet.</Card.Text>
                             <div className='card-buttons'>
                             <a href='https://damonpickett.github.io/whitsteen-project/' target='_blank' rel='noreferrer'><Button variant='dark'>Site</Button></a>
@@ -63,10 +66,10 @@ function Projects() {
 
                 <Col className='projects-col' sm={12} md={6} lg={4}>
                 <div className="card-frame">
-                <Card border='dark' style={{ width: '16rem'}}>
+                <Card border='dark'>
                         <Card.Body>
                             <Card.Title style={{ marginBottom: '20px', fontSize: '1.2em'}}>RoboPunks NFT</Card.Title>
-                            <Card.Img src={robopunks} style={{ marginBottom: '20px'}}/>
+                            <Card.Img src={robopunks} />
                             <Card.Text>My Full Mint Website allows users to connect their Ethereum wallets and mint a RoboPunks NFT token on the Goerli Testnet.</Card.Text>
                             <div className='card-buttons'>
                             <a href='https://damonpickett.github.io/full-mint-website/' target='_blank' rel='noreferrer'><Button variant='dark'>Site</Button></a>
@@ -74,16 +77,15 @@ function Projects() {
                             </div>
                         </Card.Body>
                     </Card>
-
-                    
                     </div>
                 </Col>
+
                 <Col className='projects-col' sm={12} md={6} lg={4}>
                 <div className="card-frame">
-                <Card border='dark' style={{ width: '16rem'}}>
+                <Card border='dark'>
                         <Card.Body>
                             <Card.Title style={{ marginBottom: '20px', fontSize: '1.2em'}}>Sci-fi Film Poster Club</Card.Title>
-                            <Card.Img src={scifi} style={{ marginBottom: '20px'}}/>
+                            <Card.Img src={scifi} />
                             <Card.Text>The Sci-Fi Film Poster Club allows users to connect their Ethereum wallets and mint ERC721 tokens on the Rinkeby Testnet.</Card.Text>
                             <div className='card-buttons'>
                             <a href='https://damonpickett.github.io/sffpc/#/' target='_blank' rel='noreferrer'><Button variant='dark'>Site</Button></a>
@@ -91,8 +93,6 @@ function Projects() {
                             </div>
                         </Card.Body>
                     </Card>
-
-                    
                     </div>
                 </Col>
                 
